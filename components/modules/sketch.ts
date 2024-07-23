5acc80e388eb7426c9d351deacd25074207b91d6
Katsuji <katsuji@paon.dev>
2024-07-23 23:33:27 +0900

    基本機能の追加

---
+++ components/modules/sketch.ts
@@ -0,0 +1,59 @@
+import p5 from "p5";
+
+function random(a: number, b: number): number {
+  return Math.floor(Math.random() * (b + 1 - a)) + a;
+}
+
+const sketch = (p: p5) => {
+  
+  let drop: Drop[] = [];
+
+  p.setup = () => {
+    p.createCanvas(p.windowWidth, p.windowHeight);
+    p.background(100);
+    p.noStroke();
+    p.smooth();
+    for (let i = 0; i < 600; i++) {
+      drop[i] = new Drop();
+    }
+  };
+
+  p.draw = () => {
+    p.background(0);
+    for (let i = 0; i < 600; i++) {
+      drop[i].show();
+      drop[i].update();
+    }
+  };
+
+  class Drop {
+    x: number;
+    y: number;
+    speed: number;
+    gravity: number;
+  
+    constructor() {
+      this.x = random(0, p.windowWidth);
+      this.y = random(0, -p.windowHeight);
+      this.speed = random(5, 10);
+      this.gravity = 1.35;
+    }
+  
+    show() {
+      p.noStroke();
+      p.fill(255);
+      p.ellipse(this.x, this.y, random(1, 5), random(1, 5));
+    }
+  
+    update() {
+      this.y += this.speed * this.gravity;
+  
+      if (this.y > p.windowHeight) {
+        this.y = random(0, -p.windowHeight);
+        this.gravity = 1.35;
+      }
+    }
+  }
+};
+
+export default sketch;
